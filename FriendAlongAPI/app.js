4aed146f0088e063a204e7a0d096356f4d758165
rycharlind <rycharlind@gmail.com>
2014-12-08 19:39:35 -0600

    Added Signup

--- FriendAlongAPI/app.js
+++ FriendAlongAPI/app.js
@@ -9,9 +9,88 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  ,passport = require('passport')
+  ,LocalStrategy = require('passport-local').Strategy
+  ,mongoose = require('mongoose')
+  ,bCrypt = require('bcrypt');
 
 var app = express();
+
+var User = mongoose.model('User',{
+    username: String,
+	password: String,
+	email: String,
+	gender: String,
+	address: String
+});
+
+app.configure(function() {
+	  app.use(express.static('public'));
+	  app.use(express.cookieParser());
+	  app.use(express.bodyParser());
+	  app.use(express.session({ secret: 'keyboard cat' }));
+	  app.use(passport.initialize());
+	  app.use(passport.session());
+	  app.use(app.router);
+	});
+
+passport.serializeUser(function(user, done) {
+	  done(null, user.id);
+	});
+
+	passport.deserializeUser(function(id, done) {
+	  User.findById(id, function(err, user) {
+	    done(err, user);
+	  });
+	});
+	
+
+	
+	passport.use('signup', new LocalStrategy({
+	    passReqToCallback : true
+	  },
+	  function(req, username, password, done) { 
+		// if there is no user with that email
+          // create the user
+		  mongoose.connect('mongodb://localhost/friendalong');
+		  
+          var newUser = new User();
+          // set the user's local credentials
+          newUser.username = username;
+          newUser.password = createHash(password);
+          newUser.email = req.param('email');
+          newUser.firstName = req.param('firstName');
+          newUser.lastName = req.param('lastName');
+          
+          newUser.save(function(err,item){
+        	  if (err) return console.error(err);
+        	  mongoose.disconnect();
+          });
+          
+          
+	}));
+	
+	var isAuthenticated = function (req, res, next) {
+		  if (req.isAuthenticated())
+		    return next();
+		  res.redirect('/');
+		}
+	     
+	var createHash = function(password){
+		 return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+		}
+	
+	 app.get('/signup', function(req, res){
+		    res.render('register',{});
+		  });
+		 
+		  /* Handle Registration POST */
+		  app.post('/signup', passport.authenticate('signup', {
+		    successRedirect: '/',
+		    failureRedirect: '/signup',
+		    failureFlash : false 
+		  }));
 
 // all environments
 app.set('port', process.env.PORT || 3000);
